a6d2eba892d8fbe01bd33b1e9079469656cbe483
jesuisnicolasdavid <jesuisnicolasdavid@gmail.com>
2018-05-20 17:24:04 +0000

    update package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version      = '1.0',
     packages     = find_packages(),
      package_data={
-        'myproject': ['resources/*.json']
+        'project': ['resources/*.json']
     },
     scripts      = ['script/cleaner_to_CSV.py'],
     entry_points = {'scrapy': ['settings = A308366.settings']},
